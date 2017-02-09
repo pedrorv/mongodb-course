2487068ccb34814f96ed8d96fcb643b087a74e43
Pedro Reis <pedroreisv@gmail.com>
2017-02-09 18:23:56 -0200

    Configure testing db

--- muber/app.js
+++ muber/app.js
@@ -6,7 +6,10 @@ const routes = require('./routes/routes')
 const app = express()
 
 mongoose.Promise = global.Promise
-mongoose.connect('mongodb://localhost/muber')
+
+if (process.env.NODE_ENV !== 'test') {
+  mongoose.connect('mongodb://localhost/muber')
+}
 
 app.use(bodyParser.json())
 routes(app)
